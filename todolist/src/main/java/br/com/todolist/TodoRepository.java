663902eb92c0ff2500e5da506ccea3f1b26ea154
Ana Julia <ana.francisco4@aluno.senai.br>
2023-08-11 11:21:07 -0300

    Criando endpoint para listar as tarefas

--- todolist/src/main/java/br/com/todolist/TodoRepository.java
+++ todolist/src/main/java/br/com/todolist/TodoRepository.java
@@ -1,2 +1,6 @@
-package br.com.todolist;public class TodoRepository {
+package br.com.todolist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Integer> {
 }
